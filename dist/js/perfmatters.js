64b3a090037ac16a0a2d95ab48ded6ace68f6db9
susmithagudapati <susmitha.gudapati@gmail.com>
2017-06-10 17:07:24 +0530

    fix: Run grunt after the fixes.

--- dist/js/perfmatters.js
+++ dist/js/perfmatters.js
@@ -1,5 +1,5 @@
 /*
- optimization 2017-06-08 
+ optimization 2017-06-10 
 */
 
 function logCRP(){var o=window.performance.timing,n=o.domContentLoadedEventStart-o.domLoading,t=o.domComplete-o.domLoading;document.getElementById("crp-stats").textContent="DCL: "+n+"ms, onload: "+t+"ms"}window.addEventListener("load",function(o){logCRP()});
